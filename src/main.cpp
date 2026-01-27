ffc354f342a567fe9037287f5657ba4ff6a36544
jaronie <[200~Jisaa@BALONEY MINGW64 ~$ git config --global user.name jaronieJisaa@BALONEY MINGW64 ~$ git config --global user.email>
2026-01-27 19:01:11 +0000

    Changed backdrop color.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,18 @@
-#include <bn.core.h>
-#include <bn.backdrop.h>
-#include <bn.color.h>
+#include <bn_core.h>
+#include <bn_backdrop.h>
+#include <bn_color.h>
 
+#include "bn_sprite_items_bug.h"
 
-int main() {
+int main()
+{
     bn::core::init();
 
-    bn::backdrop::set_color(bn::color(23,0,12));
+    bn::backdrop::set_color(bn::color(0,23,12));
 
-    while(true) {
+
+    while (true)
+    {
         bn::core::update();
     }
 }
