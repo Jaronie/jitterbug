9f5e880f9817b004810dac06b89213dbbb68d3b1
jaronie <[200~Jisaa@BALONEY MINGW64 ~$ git config --global user.name jaronieJisaa@BALONEY MINGW64 ~$ git config --global user.email>
2026-01-27 19:56:39 +0000

    Added sprite functionalities with vector.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include <bn_backdrop.h>
 #include <bn_color.h>
 #include <bn_sprite_ptr.h>
+#include <bn_random.h>
+#include <bn_vector.h>
+#include <bn_keypad.h>
 
 #include "bn_sprite_items_bug.h"
 
@@ -9,12 +12,32 @@ int main()
 {
     bn::core::init();
 
+    bn::random rng = bn::random();
+
     bn::backdrop::set_color(bn::color(0,23,12));
 
-    bn::sprite_ptr jeffrey = bn::sprite_items::bug.create_sprite();
+    bn::vector<bn::sprite_ptr, 50> bugs = {};
+
+    //bn::sprite_ptr jeffrey = bn::sprite_items::bug.create_sprite();
 
     while (true)
     {
+        for(bn::sprite_ptr& jeffrey : bugs)
+        {
+        bn::fixed new_x = jeffrey.x() + rng.get_fixed(-1, 1);
+        bn::fixed new_y = jeffrey.y() + rng.get_fixed(-1,1);
+
+        jeffrey.set_x(new_x);
+        jeffrey.set_y(new_y);
+        }
+
+
+        if(bn::keypad::a_pressed()){
+            bugs.push_back(bn::sprite_items::bug.create_sprite());
+
+        }
+
+        rng.update();
         bn::core::update();
     }
 }
